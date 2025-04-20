fcefc33f903b28119f8e0209e4da05e4d119a2a6
Firadi <122645437+Firadi@users.noreply.github.com>
2025-04-20 19:26:55 +0100

    initialize db connection

---
+++ src/main/java/com/project_technique/project_technique/ProjectTechniqueApplication.java
@@ -0,0 +1,13 @@
+package com.project_technique.project_technique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTechniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTechniqueApplication.class, args);
+	}
+
+}
