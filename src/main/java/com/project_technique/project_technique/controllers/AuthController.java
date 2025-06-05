b02336028a730e8e953d14e13aa49c985556c940
Firadi <122645437+Firadi@users.noreply.github.com>
2025-06-05 19:46:29 +0100

    implementing /validate end point to chech the validity of the token

--- src/main/java/com/project_technique/project_technique/controllers/AuthController.java
+++ src/main/java/com/project_technique/project_technique/controllers/AuthController.java
@@ -6,15 +6,16 @@ import com.project_technique.project_technique.dto.AuthResponse;
 import com.project_technique.project_technique.security.CustomUserDetails;
 import com.project_technique.project_technique.services.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequestMapping("/api/auth")
 @RestController
@@ -43,6 +44,22 @@ public class AuthController {
         final String role = userDetails.getEmploye().getRole().name();
 
         return ResponseEntity.ok(new AuthResponse(jwt, role));
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<?> validateToken(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or expired token");
+        }
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        return ResponseEntity.ok(Map.of(
+                "email", userDetails.getUsername(),
+                "role", userDetails.getEmploye().getRole().name(),
+                "firstName", userDetails.getEmploye().getFirstName(),
+                "lastName", userDetails.getEmploye().getLastName()
+        ));
     }
 
 }
